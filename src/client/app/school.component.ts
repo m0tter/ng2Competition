acfcea15502e8eba1042aceca2e9ff6cda3d1f71
m0tter <qldscottie@gmail.com>
2017-02-20 16:07:59 +1000

    schools list

--- src/client/app/school.component.ts
+++ src/client/app/school.component.ts
@@ -4,6 +4,9 @@ import { School }             from 'ng2Competition';
 
 import { AppService } from './app.service';
 
+interface school extends School {
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-school',
@@ -11,14 +14,19 @@ import { AppService } from './app.service';
   styleUrls: ['client/app/school.component.css']
 })
 export class SchoolComponent implements OnInit {
-  private schools: School[];
+  private schools: school[];
 
   constructor( private appService: AppService, private router: Router ) { }
 
   GetSchools(): void {
     this.appService.getSchools()
-      .then(resp => this.schools = resp as School[])
+      .then(resp => this.schools = resp as school[])
       .catch(err => this.errorHandler(err));
+  }
+
+  SchoolSelect_Clicked($index: number) {
+    this.schools[$index].selected = !this.schools[$index].selected;
+    // this.CheckButtons();
   }
 
   btnNew_Clicked(): void {
