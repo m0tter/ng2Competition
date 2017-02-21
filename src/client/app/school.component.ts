0af4c181834a380e24671854cc92995f658b5386
m0tter <qldscottie@gmail.com>
2017-02-21 16:14:43 +1000

    checkbutton added

--- src/client/app/school.component.ts
+++ src/client/app/school.component.ts
@@ -15,18 +15,30 @@ interface school extends School {
 })
 export class SchoolComponent implements OnInit {
   private schools: school[];
+  private editDisabled = true;
+  private deleteDisabled = true;
 
   constructor( private appService: AppService, private router: Router ) { }
 
-  GetSchools(): void {
+  getSchools(): void {
     this.appService.getSchools()
       .then(resp => this.schools = resp as school[])
       .catch(err => this.errorHandler(err));
   }
 
-  SchoolSelect_Clicked($index: number) {
+  schoolSelect_Clicked($index: number) {
     this.schools[$index].selected = !this.schools[$index].selected;
-    // this.CheckButtons();
+    this.checkButtons();
+  }
+
+  checkButtons(): void {
+    this.editDisabled = true;
+    this.deleteDisabled = true;
+
+    let counter = this.schools.filter( school => school.selected === true).length
+
+    if( counter === 1 ) this.editDisabled = false;
+    if( counter > 0 ) this.deleteDisabled = false;
   }
 
   btnNew_Clicked(): void {
@@ -34,7 +46,7 @@ export class SchoolComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.GetSchools();
+    this.getSchools();
   }
 
   private errorHandler(error: any) {
